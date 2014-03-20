838151a324a79d9c8c7f51394eff8a3945e96603
terekacopeland <starsxmexico@live.com>
2014-03-20 16:50:57 -0400

    commit 2

--- copeland_tereka_lab8.js
+++ copeland_tereka_lab8.js
@@ -1,1 +1,28 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+//SDI 1403
+//Lab 8
+//Tereka Copeland
+//WORKING WITH STRINGS
+
+//problem 1
+var validate = "tcopeland@fullsail.com";
+var atSymbol = checkEmail.indexOf("@")
+var 
+
+var checkEmail = function() {
+
+	if () {
+
+		console.log("Entered a valid email.");
+
+	} else {
+
+		console.log("Invalid email!");
+		
+	};
+};
+
+checkEmail();
+
+//problem 2
