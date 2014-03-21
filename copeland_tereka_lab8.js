a4f99b8fce375c5d022c123eb6ce2def0d93e396
terekacopeland <starsxmexico@live.com>
2014-03-20 23:31:59 -0400

    problem2

--- copeland_tereka_lab8.js
+++ copeland_tereka_lab8.js
@@ -6,15 +6,20 @@
 //WORKING WITH STRINGS
 
 //problem 1
-var validate = "tcopeland@fullsail.com";
-var atSymbol = checkEmail.indexOf("@")
-var 
+/*var validate = "tcopeland@fullsail.com";
+var atSymbol = checkEmail.indexOf("@");
+var space = checkEmail.indexOf(" ");
+var period = checkEmail.indexOf(".")
 
 var checkEmail = function() {
 
-	if () {
+	if (atSymbol > 1) {
 
-		console.log("Entered a valid email.");
+		console.log("only 1 @ symbol");
+		
+		if (period > 1)
+		
+		console.log("only 1 period")
 
 	} else {
 
@@ -23,6 +28,24 @@ var checkEmail = function() {
 	};
 };
 
-checkEmail();
+checkEmail();*/
 
 //problem 2
+
+var namesStartJ = function() {
+
+	
+	var namesStartJ = ["Jimmy/ John/ Joseph/ Jack"];
+
+	for (var i = 0; i < namesStartJ.length; i++) {
+
+		console.log("For guys have names that Start with 'J' " + namesStartJ[i] + ".");
+
+
+	};
+
+	
+
+};
+
+namesStartJ();
